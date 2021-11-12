ab55bebdb494d21c72d1d848fc01dc0ec9164e2e
JDgomez2002 <gom21429@uvg.edu.gt>
2021-11-12 09:23:52 -0600

    MAIN

---
+++ Controlador.java
@@ -0,0 +1,22 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//Grupo 9:
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Abner Iván García Alegría
+//21285
+//Sección 11
+//Actividad: Laboratorio 4
+
+/**
+ * Clase Controlador
+ * 
+ * @author Grupo 8
+ * @version Controlador 1.1
+ */
+
+public class Controlador {
+    
+}
---
+++ MAIN.java
@@ -0,0 +1,24 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//Grupo 9:
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Abner Iván García Alegría
+//21285
+//Sección 11
+//Actividad: Laboratorio 4
+
+/**
+ * Clase Main
+ * 
+ * @author Grupo 8
+ * @version MAIN 1.1
+ */
+
+public class MAIN{
+    public static void main(String[] args) {
+        
+    }
+}
